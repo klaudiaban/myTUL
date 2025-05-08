585ee0a69d5dcd09ef9352bbcf572e975cac1632
Mieszko <najgorzej111@gmail.com>
2025-05-08 22:14:04 +0200

    minor changes to database files

--- database_admin.py
+++ database_admin.py
@@ -20,7 +20,7 @@ safe_key = str(timestamp)
 data = {"place": "Biblioteka", "amount": random.randint(0, 15)}
 rooms ={
     "Biblioteka": db.reference('/AdminData/AllTime/Biblioteka'),
-    "Lodex": db.reference('/AdminData/AllTime/Lodex')
+    "Lodex": db.reference('/AdminData/AllTime/Lodex'),
 }
 
 # realtime data update
@@ -48,6 +48,113 @@ stats_data = {
                         "amount": 1,
                     },
                 "8":
+                    {
+                        "amount": 3,
+                    },
+                "9":
+                    {
+                        "amount": 3,
+                    },
+                "10":
+                    {
+                        "amount": 3,
+                    },
+                "11":
+                    {
+                        "amount": 3,
+                    },
+                "12":
+                    {
+                        "amount": 3,
+                    },
+                "13":
+                    {
+                        "amount": 3,
+                    },
+                "14":
+                    {
+                        "amount": 3,
+                    },
+                "15":
+                    {
+                        "amount": 3,
+                    },
+                "16":
+                    {
+                        "amount": 3,
+                    },
+                "17":
+                    {
+                        "amount": 3,
+                    },
+                "18":
+                    {
+                        "amount": 3,
+                    },
+                "19":
+                    {
+                        "amount": 3,
+                    },
+                "20":
+                    {
+                        "amount": 3,
+                    },
+            },
+        "Tuesday":
+            {
+                "7":
+                    {
+                        "amount": 1,
+                    },
+                "8":
+                    {
+                        "amount": 3,
+                    },
+                "9":
+                    {
+                        "amount": 3,
+                    },
+                "10":
+                    {
+                        "amount": 3,
+                    },
+                "11":
+                    {
+                        "amount": 3,
+                    },
+                "12":
+                    {
+                        "amount": 3,
+                    },
+                "13":
+                    {
+                        "amount": 3,
+                    },
+                "14":
+                    {
+                        "amount": 3,
+                    },
+                "15":
+                    {
+                        "amount": 3,
+                    },
+                "16":
+                    {
+                        "amount": 3,
+                    },
+                "17":
+                    {
+                        "amount": 3,
+                    },
+                "18":
+                    {
+                        "amount": 3,
+                    },
+                "19":
+                    {
+                        "amount": 3,
+                    },
+                "20":
                     {
                         "amount": 3,
                     },
