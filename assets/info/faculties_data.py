59d98b52b3d6a4701f48b037abf3ce33de1a44b9
klaudiaban <banasiewicz.klaudia2005@gmail.com>
2025-05-31 15:29:40 +0200

    update faculties_data.py and gitignore

---
+++ assets/info/faculties_data.py
@@ -0,0 +1,42 @@
+faculties = {
+    "BAIS": {
+        "website": "https://bais.p.lodz.pl/en",
+        "news": "https://p.lodz.pl/en/about-tul/news"
+    }, 
+    "BINOZ": {
+        "website": "https://binoz.p.lodz.pl/en",
+        "news": "https://binoz.p.lodz.pl/en/taxonomy/term/1"
+    }, 
+    "Chemistry": {
+        "website": "https://chemia.p.lodz.pl/",
+        "news": "https://chemia.p.lodz.pl/wydzial/aktualnosci"
+    }, 
+    "FTIMS": {
+        "website": "https://ftims.p.lodz.pl/en",
+        "news": "https://ftims.p.lodz.pl/en/faculty/news"
+    }, 
+    "IFE": {
+        "website": "https://www.ife.p.lodz.pl/en",
+        "news": "https://www.ife.p.lodz.pl/en/news"
+    }, 
+    "Mechanical Engineering": {
+        "website": "https://mechaniczny.p.lodz.pl/en",
+        "news": "https://mechaniczny.p.lodz.pl/en/news"
+    }, 
+    "WEEIA": {
+        "website": "https://weeia.p.lodz.pl/en",
+        "news": "https://weeia.p.lodz.pl/en/faculty/news"
+    }, 
+    "WIPOS": {
+        "website": "https://wipos.p.lodz.pl/en",
+        "news": "https://p.lodz.pl/en/about-tul/news"
+    }, 
+    "WOIZ": {
+        "website": "https://woiz.p.lodz.pl/en",
+        "news": "https://p.lodz.pl/en/about-tul/news"
+    }, 
+    "WTMiWT": {
+        "website": "https://style.p.lodz.pl/",
+        "news": "https://style.p.lodz.pl/aktualnosci"
+    }
+}
